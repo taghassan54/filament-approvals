1bc296476a2d173c9af5b4ecce22c726b5a47e5d
Tony Partridge <tony@riela-yachts.com>
2024-04-11 10:14:41 +0100

    Fix Approval status column error where no approvalStatus exists Add language strings to Approval Status Column

--- resources/lang/en/approvals.php
+++ resources/lang/en/approvals.php
@@ -2,5 +2,9 @@
 
 // translations for EightyNine/Approval
 return [
-    //
+    'approval_by_prefix' => 'By',
+    'approval_complete' => 'Complete',
+    'approval_incomplete' => 'Incomplete',
+    'approval_in_process' => 'In Process',
+    'approval_status_does_not_exist' => 'No Status Exists',
 ];
--- resources/views/tables/columns/approval-status-column.blade.php
+++ resources/views/tables/columns/approval-status-column.blade.php
@@ -1,18 +1,28 @@
 <div>
-    <p class="px-3">
-        <small>
-            {{ $getRecord()->approvalStatus->status }} by
-            @if ($getRecord()->lastApproval)
-                {{ $getRecord()->lastApproval->approver_name }}
-            @else
-                {{ $getRecord()->createdBy()->name }}
-            @endif
-        </small>
-    </p>
-    <p class="px-3 text-xs">
-        <small>
-            {{ $getRecord()->isApprovalCompleted() ? 'Completed' : 'In process' }}
-        </small>
-    </p>
-
+    @if($getRecord()->approvalStatus)
+        <span class="px-3 py-1 bg-green-200 text-green-800 rounded-full text-xs">
+            {{ $getRecord()->approvalStatus->status }}
+        </span>
+        <p class="px-3">
+            <small>
+                {{ $getRecord()->approvalStatus->status }} {{ __('filament-approvals::approvals.approval_by_prefix') }}
+                @if ($getRecord()->lastApproval)
+                    {{ $getRecord()->lastApproval->approver_name }}
+                @else
+                    {{ $getRecord()->createdBy()->name }}
+                @endif
+            </small>
+        </p>
+        <p class="px-3 text-xs">
+            <small>
+                {{ $getRecord()->isApprovalCompleted() ?
+                    __('filament-approvals::approvals.approval_complete') :
+                    __('filament-approvals::approvals.approval_in_process') }}
+            </small>
+        </p>
+    @else
+        <span class="px-3 py-1 bg-gray-200 text-gray-800 rounded-full text-xs">
+            {{ __('filament-approvals::approvals.approval_status_does_not_exist') }}
+        </span>
+    @endif
 </div>
